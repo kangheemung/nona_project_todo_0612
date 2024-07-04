67bcfeb5d44ccfb7dbe604f322625d6ebb301aae
kurotamashi30 <kurotamashi30@gmail.com>
2024-07-04 15:51:07 +0900

    test

--- main.js
+++ main.js
@@ -32,18 +32,48 @@
  //   console.log(taskList);
 // }
     let taskInput = document.getElementById("text_input");
-    let taskList = [];
+    let taskList = [];//削除
+    let Completed = true;
+    let taps = document.querySelectorAll(".task-tabs div" )//여러개 선택
+    let mode = "All";
+    let filterList = [];
+    let add_button = document.getElementById("add_button");
+    let under_Line = document.getElementById("under-line")
     console.log(taskInput);
-    taskInput.addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
+
+    function underline(e) {
+        under_Line.style.transition = "all 0.5s"; 
+        under_Line.style.left = e.offsetLeft + "px";
+        under_Line.style.width = e.offsetWidth + "px";
+        under_Line.style.top = e.offsetTop + e.offsetHeight + "px";
+      }
+      taps.forEach((task) => {
+        task.addEventListener("click", function(e) {
+            underline(e.target);
+        });
+    });
+     add_button.addEventListener("click",addTask);
+     for(let i = 1; i< taps.length; i++){
+        taps[i].addEventListener("click",function(e){
+            filter(e);
+        });
+    }
+
+    taskInput.addEventListener("keypress", function(e) {
+        if (e.key === "Enter") {
             addTask();
         }
     });
-    let add_button = document.getElementById("add_button");
-    add_button.addEventListener("click",addTask);
 
+    console.log(taps)
 function addTask(){
-        let task={
+    let taskContent = taskInput.value.trim(); // Trim any leading or trailing whitespace
+
+    if (!taskContent) {
+        taskInput.placeholder = "오늘 할일을 적어주세요!";
+        return;
+    }
+        let task = {
             id: randomIDGenerate(),//유니크한 값을 넣는것이 중요하다.
             taskContent: taskInput.value,
             isComplete: false,
@@ -54,33 +84,43 @@ function addTask(){
         render();
         taskInput.value = "";
     }
-
+//UI부분
     function render() {
+         //taskList를 돌리고 있습니다.
+        //fillter정보는 없어요..
+        //내가 선택한 탭에 따라서
+        let list = [];
+        if (mode === "All") {
+            list = taskList;
+        } else if (mode === "Not_Done") {
+            list = filterList;
+        }
+        else if (mode === "Done") {
+        list = filterList;
+    }
+        //리스트를 달리 보여준다.
+        //
+        //allList
+
         let resultHTML = "";
-        for (let i = 0; i < taskList.length; i++) {
-            if(taskList[i].isComplete === true){
+        for (let i = 0; i < list.length; i++) {
+            if(list[i].isComplete === true){
+                // 완료된 할일 스타일링
                 resultHTML+=
                 `<div class = "taskzoon">
-                    <div class="task_done">${taskList[i].taskContent}</div>
+                    <div class="task_done">${list[i].taskContent}</div>
                     <div>
-                        <button onclick="checkTask('${taskList[i].id}')">
-                        <img class= "image" src="images/free-icon-checking.png" >
-                        </button>
-                        <button  onclick="deleteTask('${taskList[i].id}')">
-                        <img class= "image" src="images/delete.png" >
-                        </button>
+                        <img onclick="checkTask('${list[i].id}')" class= "image" src="images/free-icon-checking.png" >
+                        <img onclick="deleteTask('${list[i].id}')" class= "image" src="images/delete.png" >
                     </div>
                 </div>`;
             }else{
             resultHTML += `
                <div class = "task">
-                    <div class = "task_text">${taskList[i].taskContent}</div>
+                    <div class = "task_text">${list[i].taskContent}</div>
                     <div>
-                        <button  onclick="checkTask('${taskList[i].id}')">
-                        <img class= "image" src="images/free-icon.png" >
-                        </button>
-                        <button  onclick="deleteTask('${taskList[i].id}')">
-                        <img class= "image" src="images/delete.png" ></button>
+                        <img onclick="checkTask('${list[i].id}')" class= "image" src="images/free-icon.png" >
+                        <img onclick="deleteTask('${list[i].id}')" class= "image" src="images/delete.png" >
                     </div>
                 </div>`;
         }
@@ -98,31 +138,83 @@ function checkTask(id) {
     console.log("ok");
     console.log("id:",id);//ok
     // Example: taskElement.style.textDecoration = 'line-through'
-    for( let i=0; i<taskList.length; i++){
+    for( let i=0; i < taskList.length; i++){
         if (taskList[i].id === id){
             taskList[i].isComplete = !taskList[i].isComplete;
-            //true변환!taskList[i].isComplete;스위치같은기능
+
+            if (mode === "Done" && taskList.find(task => task.id === id).isComplete === false) {
+
+                mode = "All"; // 모든 모드로 변경
+                render(taskList);
+            } else if (mode === "Not_Done") {
+                filterList = taskList.filter(task => !task.isComplete);
+                render(filterList);
+            }else if (mode === "All" && taskList.isComplete === false) {
+
+                render(taskList);
+            }
+
+            //true변환!list[i].isComplete;스위치같은기능
             break;
         }
+
     }
     render();//함수 호출!// 끝난거로 간주하고 밑줄 보여주기
-    console.log(taskList);
+
 }
 function randomIDGenerate(){
       //id를 부여해준다.(generate random did javascript 로 검색해본다ㅣ)
       return '_' + Math.random().toString(36).substr(2, 9);
 }
 
-function deleteTask(id) {
-    console.log("delete");
-    for (let i = 0; i < taskList.length; i++) {
-        if (taskList[i].id === id) {
-            taskList.splice(i, 1);
-            break;
-        }
+function filter(e){
+
+  mode = e.target.id;
+
+    console.log("filter", e.target.id);
+    //3case
+    if (mode === "All"){
+        //전체리스트를 보여준다
+        render(taskList);
+    } else if (mode === "Not_Done"){
+        //진행중인 아이템을 보여준다.
+        // task.isComplete=false
+            //  for (let i = 0; i < list.length; i++){
+            //  if (list[i].isComplete === false){
+            //    filterList.push(list[i]);
+            //  }
+        console.log("進行中",filterList);
+        filterList = taskList.filter(task => !task.isComplete);
+        render(filterList);
+        render();
+    } else if (mode === "Done"){
+        //끝나는케이스
+         // task.isComplete=ture
+         filterList = taskList.filter(task => task.isComplete);
+         render(filterList);
+         render();
     }
-    render();
 }
+function deleteTask(id) {
+    let confirmDelete = confirm("삭제합니다.");
+
+    if (confirmDelete) {
+        for (let i = 0; i < taskList.length; i++) {
+            if (taskList[i].id === id) {
+                taskList.splice(i, 1);
+                
+                if (mode === "Not_Done" || mode === "Done") {
+                    filterList = filterList.filter(task => task.id !== id);
+                }
+                
+                break;
+            }
+        }
+
+        render(mode === "All" ? taskList : filterList);
+    }
+}
+
 let darkModeEnabled = false;
 const toggleDarkModeButton = document.getElementById("toggle-dark-mode");
 const modeIcon = document.querySelector("#toggle-dark-mode img");
